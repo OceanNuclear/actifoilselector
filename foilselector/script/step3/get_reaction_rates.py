9066ee9e2ef71e67263e1864296baf03108d7425
OceanNuclear <annoyingamygdala@gmail.com>
2021-07-21 21:25:18 +0100

    changed the content of get_reaction_rates.py

--- foilselector/script/step3/get_reaction_rates.py
+++
@@ -1,9 +0,0 @@
-# PARAMETERS
-GUESS_MATERIAL = False
-IRRADIATION_DURATION = 3600 # spread the irradiation power over the entire course of this length
-TRANSIT_DURATION = 5*60
-MEASUREMENT_DURATION = 3600
-MAX_AREA = 6.25 # cm^2
-ENRICH_TO_100_PERCENT = False # allowing enrichment means 100% of that element being made of the specified isotope only
-
-CONDENSED_DECAY_INFO_FILE = 'decay_counts.json'
