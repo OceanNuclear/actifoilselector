ccfc0836bc19ecfa8bcccb1a912b36f17c7e099e
ocean <OceanWongUK@gmail.com>
2024-11-16 23:04:37 +0000

    response matrix extraction complete

--- foilselector/script/step2.py
+++ foilselector/script/step2.py
@@ -19,6 +19,7 @@ A nuance that I have to clear up: if a(n advaned) user _knows_ that there's a sp
 """
 
 import json
+from pathlib import Path
 from collections import defaultdict
 from tqdm import tqdm
 from numpy import array as ary
@@ -33,14 +34,114 @@ from foilselector.openmcextension import * # collapse_single_xs
 from foilselector.openmcextension.table import Tab1DExtended
 from foilselector.openmcextension.extended_io import serialize_radiation_dict, deserialize_radiation_dict
 from foilselector.constants import BARN
+from foilselector.generic import sorted_dict
 from foilselector.simulation.decay.bateman import mat_exp_num_decays
 from foilselector.simulation.decay import linearize_decay_chain, build_decay_chain_tree
 
 default_gamma_energy_limits_keV = [20, 4600]
 
 
+def load_relevant_xs_and_decay_info(composition_dict: dict, libraries: list[Path]) -> tuple[dict, dict]:
+    """
+    Open the libraries, load in only the relevant cross-sections and decay data.
+
+    Returns
+    -------
+    xs_dict: dict[str, openmc.data.Tabulated1D]
+        See sparsely_load_xs_and_decay_dict
+    decay_dict: dict[str, dict]
+        See sparsely_load_xs_and_decay_dict
+    """
+    # stage 3.1: find what isotopes need to be extracted.
+    isotope_of_interest = set()
+    for isotopes in composition_dict.values():
+        for iso in isotopes.keys():
+            atnum_and_massnum = commonname_to_atnum_massnum(iso)
+            isotope_of_interest.add(atnum_and_massnum)
+    # stage 3.2: extract them
+    return sparsely_load_xs_and_decay_dict(isotope_of_interest, libraries)
+
+def calculate_response_matrix(
+    foil_composition:dict,
+    xs_dict: dict,
+    decay_dict: dict,
+    gs_array:np.ndarray,
+    a, b, c
+) -> tuple[dict, dict]:
+    """
+    For each gamma-ray energy or gamma-ray continuum, sum up the cross-sections of
+    all of the pathways that can create it. Store these gamma:cross-sections in two
+    dictionaries, one for the discrete lines, another for the continua.
+
+    Parameters
+    ----------
+    foil_composition: dict[str, float]
+        fraction (float) of each isotope (name of isotope stored as a str) in the foil.
+    xs_dict, decay_dict:
+        see `sparsely_load_xs_and_decay_dict`
+    gs_array:
+        float array of shape (len(gs_array), 2) storing the lower and upper bounds of
+        each neutron group's energy.
+    a, b, c:
+        Times when irradiation stops, acquisition starts, and acquisition stops.
+        See `mat_exp_num_decays`.
+
+    Returns
+    -------
+    this_foil: dict[DiscreteRadiation, np.ndarray[float]]
+        cross-section inducing each gamma-line, indexed by the gamma-line (energy and
+        intensity) and its source, stored as a DiscreteRadiation.
+
+    this_background: dict[ContinuousRadiationDistribution, np.ndarray[float]]
+        cross-section inducing each gamma-continuum, indexed by the gamma-continuum and
+        its source, stored as a ContinuousRadiationDistribution.
+    """
+
+    def xs_template_generator() -> np.ndarray[float]:
+        """
+        For creating an empty array representing the cross-section for generating a
+        single count in whichever gamma-line of interest.
+        """
+        return np.zeros(len(gs_array), dtype=float)
+
+    this_foil = defaultdict(xs_template_generator)
+    this_background = defaultdict(xs_template_generator)
+    for isotope, atomic_fraction in foil_composition.items():
+        for rx_name, rx_xs in tqdm(
+                reactions_matching(xs_dict, isotope).items(),
+                desc=f"Calculating all possible reactions for reactant={isotope} and all decay pathways for each reaction.",
+                leave=False
+            ):
+            decay_pathways = linearize_decay_chain(build_decay_chain_tree(decay_dict, rx_name.split("-")[1]))
+            for pathway in decay_pathways:
+                # calculate how many decays of PRODUCT are measured per REACTANT ATOM
+                # initially present in the foil when irradiated by 1 cm^-2 s^-1 flux
+                # in each bin from time t=0-a seconds, and then measured from time
+                # t= b-c seconds.
+                decay_correction_factor = mat_exp_num_decays(
+                    pathway.branching_ratios,
+                    pathway.decay_constants,
+                    a, b, c
+                )
+                scaled_collapsed_xs = (
+                    collapse_single_xs(rx_xs, gs_array)
+                    * BARN
+                    * atomic_fraction
+                    * decay_correction_factor
+                )
+                for peak_energy, peak_intensity, source in pathway.discrete_photon_spectrum:
+                    this_foil[(peak_energy, source)] += scaled_collapsed_xs * peak_intensity
+                for background_dist, source in pathway.background_photon_spectrum:
+                    # the intensity correction value of the background is already built into background_dist
+                    this_background[(background_dist, source)] += scaled_collapsed_xs
+    return this_foil, this_background
+
 def main(
-    composition, library, irradiation_duration, transit_duration, measurement_duration
+    composition: dict,
+    libraries: list[Path],
+    irradiation_duration: float,
+    transit_duration: float,
+    measurement_duration: float
 ):
 
     # stage 1: read outputs of step1.
@@ -56,57 +157,24 @@ def main(
 
     save_atomic_composition_json(processed_composition)  # for future reference
 
+    xs_dict, decay_dict = load_relevant_xs_and_decay_info(processed_composition, libraries)
     # TODO: rewrite this stage without openmc, and possibly implement an alternative using FISPACT-II.
-    # stage 3.1: find what isotopes need to be extracted.
-    isotope_of_interest = set()
-    for isotopes in processed_composition.values():
-        for iso in isotopes.keys():
-            atnum_and_massnum = commonname_to_atnum_massnum(iso)
-            isotope_of_interest.add(atnum_and_massnum)
-    # stage 3.2: extract them
-    xs_dict, decay_dict = sparsely_load_xs_and_decay_dict(isotope_of_interest, library)
 
-    def xs_template_generator():
-        """
-        For creating an empty array representing the cross-section for generating a
-        single count in whichever gamma-line of interest.
-        """
-        return np.zeros(len(gs_array), dtype=float)
 
     every_foil_response_matrix = {}
-    for foil_name, foil_comp in tqdm(processed_composition.items(), desc="Processing every foil individually..."):
-        # TODO: double tqdm here.
-        this_foil = defaultdict(xs_template_generator)
-        this_background = defaultdict(xs_template_generator)
-        for isotope, atomic_fraction in foil_comp.items():
-            print(isotope, "is being processed")
-            for rx_name, rx_xs in reactions_matching(xs_dict, isotope).items():
-                decay_pathways = linearize_decay_chain(build_decay_chain_tree(decay_dict, rx_name.split("-")[1]))
-                for pathway in decay_pathways:
-                    # calculate how many decays of PRODUCT are measured per REACTANT ATOM
-                    # initially present in the foil when irradiated by 1 cm^-2 s^-1 flux
-                    # in each bin from time t=0-a seconds, and then measured from time
-                    # t= b-c seconds.
-                    decay_correction_factor = mat_exp_num_decays(
-                        pathway.branching_ratios,
-                        pathway.decay_constants,
-                        irradiation_duration,
-                        irradiation_duration+transit_duration,
-                        irradiation_duration+transit_duration+measurement_duration,
-                    )
-                    scaled_collapsed_xs = (
-                        collapse_single_xs(rx_xs, gs_array)
-                        * BARN
-                        * atomic_fraction
-                        * nom(pathway.branching_fraction)
-                        * decay_correction_factor
-                    )
-                    for peak_energy, peak_intensity, source in pathway.discrete_photon_spectrum:
-                        this_foil[(peak_energy, source)] += scaled_collapsed_xs * peak_intensity
-                    for background_dist, source in pathway.background_photon_spectrum:
-                        # the intensity correction value of the background is already built into background_dist
-                        this_background[(background_dist, source)] += scaled_collapsed_xs
+    for foil_name, foil_comp in tqdm(processed_composition.items(), desc="Processing each foil individually"):
+        this_foil, this_background = calculate_response_matrix(
+            foil_comp,
+            xs_dict,
+            decay_dict,
+            gs_array,
+            irradiation_duration,
+            irradiation_duration+transit_duration,
+            irradiation_duration+transit_duration+measurement_duration,
+        )
+
         every_foil_response_matrix[foil_name] = sorted_dict(this_foil)
+        # can't sort gamma-continua against each other, so no sorting on this_background.
         every_foil_background[foil_name] = this_background
 
     # Store response matrices and background spectra response matrices
@@ -118,10 +186,3 @@ def main(
         json.dump(serialize_radiation_dict(every_foil_response_matrix), j)
     print("Written response and background radiation, Done!")
 
-    gamma_energy_limits = np.array(default_gamma_energy_limits_keV) * keV
-    # if gamma_energy_limits_keV:
-    #     gamma_energy_limits = np.array(sorted(gamma_energy_limits_keV)) * keV
-    # ^ we must make sure to extract the max sigma from the the raw xs before collapsing it to the right group structure.
-
-    # This is because the process of collapsing it to the appropriate group structure destroys that information.
-    # save the rest of the useful informations into files. Needed in step 3+
