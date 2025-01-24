ad669f80a2b67f8ef12130d778e7e3d9150b99b3
ocean <OceanWongUK@gmail.com>
2025-01-24 22:14:31 +0000

    ruff formatted __init__.py

--- foilselector/__init__.py
+++ foilselector/__init__.py
@@ -1,25 +1,14 @@
-# nested folders consisting of instructions
-from foilselector import fluxconversion
-from foilselector import openmcextension
-from foilselector import physicalparameters
-from foilselector import optimizer
-from foilselector import selfshielding
-from foilselector import simulation
+"""Init file."""
 
-# the script folder
-from foilselector import script
-
-# single level python program
-from foilselector import constants
-from foilselector import foldermanagement
-from foilselector import reactionnaming
-
-"""
-Question to ask myself:
-Software name and purpose:
-- What environment does the software run in?
-- How do people interact with it?
-- Why do people use it?
-- What features of the software have been affected by these factors?
-- If the software needed to be used in a different environment, what difficulties might there be?
-"""
+from foilselector import (
+    constants,
+    fluxconversion,
+    foldermanagement,
+    openmcextension,
+    optimizer,
+    physicalparameters,
+    reactionnaming,
+    script,
+    selfshielding,
+    simulation,
+)
