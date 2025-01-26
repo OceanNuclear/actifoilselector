86297bc014be5148c3e3cf67b1d9a1549260f36a
ocean <OceanWongUK@gmail.com>
2025-01-26 02:34:10 +0000

    Updated one __init__.py file to test.

--- foilselector/fluxconversion/__init__.py
+++ foilselector/fluxconversion/__init__.py
@@ -1,16 +1,16 @@
 from foilselector.fluxconversion.convert import (
-    flux_conversion,
     convert_arbitrary_gs_from_means,
+    flux_conversion,
 )
-from foilselector.fluxconversion.smartconvert import scale_to_eV_interactive, ask_for_gs
 from foilselector.fluxconversion.filereading import list_dir_csv, open_csv
 from foilselector.fluxconversion.interactions import (
-    get_column_interactive,
     ask_question,
     ask_yn_question,
+    get_column_interactive,
 )
 from foilselector.fluxconversion.schemes import (
     get_interpolation_scheme,
     histogramic,
     loglog,
 )
+from foilselector.fluxconversion.smartconvert import ask_for_gs, scale_to_eV_interactive
