2d681f1da8aa2d32d3f3e58bf7cc6ead35045039
ocean <OceanWongUK@gmail.com>
2025-01-26 00:19:03 +0000

    ruff formatted interactions.py

--- foilselector/fluxconversion/interactions.py
+++ foilselector/fluxconversion/interactions.py
@@ -1,25 +1,20 @@
-"""
-functions used to interact with the user
-"""
+"""Functions used to "talk" to the user."""
 
-from os.path import join, basename
-from typing import TYPE_CHECKING
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 
 from foilselector.fluxconversion.filereading import open_csv
 
-if TYPE_CHECKING:
-    import pandas as pd
-
-__all__ = ["ask_yn_question", "ask_question", "get_column_interactive"]
+__all__ = ["ask_question", "ask_yn_question", "get_column_interactive"]
 
 
 def ask_yn_question(question: str):
     """
-    Ask a yes no question
-    parameters
+    Ask a yes no question.
+
+    Parameters
     ----------
     question : string containing the question to be displayed.
 
@@ -34,34 +29,41 @@ def ask_yn_question(question: str):
     yes: ["yes", "y", "Yes", "YES", "Y"]
     no: ["no", "n", "No", "NO", "N"]
 
-    returns
+    Returns
     -------
     Boolean (True/False)
     """
     while True:
         answer = input(question + "('y','n') ")
-        if answer.lower() in ["yes", "y"]:
+        if answer.lower() in {"yes", "y"}:
             return True
-        elif answer.lower() in ["no", "n"]:
+        if answer.lower() in {"no", "n"}:
             return False
-        else:
-            print(f"Option '{answer}' not recognized; please retry: ")
+        print(f"Option '{answer}' not recognized; please retry: ")
 
 
 def ask_question(question: str, expected_answer_list: list[str], *, check: bool = True):
     """
     Ask the user a multiple choice question.
+
     Parameters
     ----------
-    question : entire string of the question.
-    expected_answer_list : list of strings which are the expected answers.
-                           If the user gives an answer that isn't included the list, and check=True,
-                           then their answer will be discarded and the quesiton will be asked again until a matching answer is found.
-    check : see expected_answer_list
+    question:
+        entire string of the question.
+
+    expected_answer_list:
+        list of strings which are the expected answers.
+        If the user gives an answer that isn't included the list, and check=True,
+        then their answer will be discarded and the quesiton will be asked again until
+        a matching answer is found.
+
+    check:
+        see expected_answer_list
 
     Returns
     -------
-    answer given by user
+    :
+        answer given by user
     """
     while True:
         answer = input(f"{question} ")
@@ -72,17 +74,38 @@ def ask_question(question: str, expected_answer_list: list[str], *, check: bool 
     return answer
 
 
+def _check_is_numpy_numeric(data: np.ndarray):
+    """
+    Ensure that a numpy array is indeed a numeric type (int64, float64, etc.).
+
+    Parameters
+    ----------
+    data:
+        A numpy array.
+
+    Raises
+    ------
+    ValueError
+        Raised if the data array is not of a numeric data type.
+    """
+    if not np.issubdtype(data.dtype, np.number):
+        raise ValueError("Expected numerical data, got " f"{type(data.dtype)}.")
+
+
 def get_column_interactive(
     directory: Path,
     datatypename: str,
     *,
     first_time_use: bool = False,
     output_full_file_path: bool = False,
-    file_path_given: Path = None,
-):
+    file_path_given: Path | None = None,
+) -> np.ndarray[float] | tuple[np.ndarray[float], str]:
     """
-    Ask the user for the column in a csv file within the specified {directory}, containing the {datatypename}.
+    Ask the user for the column in a csv file within the specified {directory},
+    containing the {datatypename}.
     Keep asking until it is successfully found.
+    Data must be numeric in type.
+
     Parameters
     ----------
     directory:
@@ -117,25 +140,23 @@ def get_column_interactive(
         exit_status: int
             If exit status is non-zero, then an error has occured
         """
-
         try:
+            file_path = Path(file_path)
             df, col = open_csv(file_path)
-            print(f"Opened {basename(file_path)}\n", df.head(), "\n...")
+            print(f"Opened {file_path.name}\n", df.head(), "\n...")
             colname = input(
-                f"Please input the index/name of the column where {datatypename} is/are contained.\n(column name options include {list(col)}) "
+                f"Please input the index/name of the column where {datatypename} is/are "
+                "contained.\n(column name options include {list(col)}) ",
             )
-            if colname in col:
-                col_i = colname
-            else:
-                col_i = col[int(colname)]
+            col_i = colname if colname in col else col[int(colname)]
+            _check_is_numpy_numeric(df[col_i].to_numpy()[0])
+
             return df[col_i], 0
 
         except FileNotFoundError as e:
             print(e, f"Please enter a valid file in {directory}.")
         except ValueError as e:
             print(e, "Perhaps the column name/ index is wrong. Please try again.")
-        except Exception as e:
-            print(e, "Please try again.")
 
         return None, 1
 
@@ -150,11 +171,11 @@ def get_column_interactive(
         if not first_time_use:
             prompt += "(It may be the same file as previously used.)"
         fname = input(f"{prompt} ")
-        full_file_path = join(directory, fname)
+        full_file_path = Path(directory, fname)
         dataseries, exit_status = one_loop(full_file_path)
 
     print(f"Data chosen =\n{dataseries.head()}\n...")
 
     if output_full_file_path:
-        return dataseries.values, full_file_path
-    return dataseries.values
+        return dataseries.to_numpy(), full_file_path
+    return dataseries.to_numpy()
